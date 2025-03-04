a48e81caa9a934fa2f28f70031a535bf4245f29b
ESuances <erick.s.suances.arc@gmail.com>
2025-03-04 10:41:09 -0600

    Day 4
    Retomando esto de aprender programacion, algo importante es tomar las cosas con calma e ir avanzando, aunque sea poco a poco

--- PycharmProjects/To-Do-List-Pyhton/main.py
+++ PycharmProjects/To-Do-List-Pyhton/main.py
@@ -1,7 +1,7 @@
 to_dos = []
 
 while True:
-    desicion = input("Type add or show, or exit: ")
+    desicion = input("Type add or show, edit or exit: ")
     desicion = desicion.strip()
 
     match desicion:
@@ -11,6 +11,12 @@ while True:
         case "show":
             for x in to_dos:
                 print(x.capitalize())
+        case "edit":
+            edit_to_do = int(input("Enter the number of the to do you wish to edit: "))
+            edit_to_do = edit_to_do - 1
+            new_to_do = input("Enter the new to do: ")
+            to_dos[edit_to_do] = new_to_do
+            print(to_dos[edit_to_do])
         case "exit":
             break
         case _:
