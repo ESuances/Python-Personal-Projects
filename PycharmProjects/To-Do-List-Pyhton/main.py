5ca22003a443d6453079fa788f9e29b10988987a
ESuances <erick.s.suances.arc@gmail.com>
2025-03-08 21:46:12 -0600

    Day 8
    Changes to the code, mainly finished the managing non-binary files, I feel awesome at this point, It is just amazing how much can be done just by staying consistant and this is nothing yet.

--- PycharmProjects/To-Do-List-Pyhton/main.py
+++ PycharmProjects/To-Do-List-Pyhton/main.py
@@ -6,40 +6,64 @@ while True:
     match desicion:
         case "add":
             to_do = input("Enter a to do: ") + "\n"
-            file = open('to_dos.txt','r') # We create a read first to get its values
-            file.close() # It is better to close the file once we already have the information stored in the file variable
-            to_dos = file.readlines() # We assign the values that were already there to the list
+            # GOOD THING TO REMEMBER: Every time you read a file, the pointer is set right after the last character
+            # Which means, if you try to read two times in a single with open .... , the second one won't work, because the pointer would have to detect added text after previous code was executed.
+            # This of course would be a good way to find out if a non-binary file is being edited while the script is running.
+            # Anything else, please, only use the read option once and preferably store it in a variable so that you have it in the code after that.
+            with open("to_dos.txt", "r") as file:
+                to_dos = file.readlines()
             to_dos.append(to_do) # We provide the added to do to the RAM
-            file = open('to_dos.txt','w')  # We open the file again, this time to write the added to do.
-            file.writelines(to_dos) # We write the file with all of the new information
-            file.close() # Again, once we write the new info in the file, we once again close it.
+            with open("to_dos.txt", "w") as file:
+                file.writelines(to_dos)
         case "show":
-            file = open('to_dos.txt', 'r')
-            to_dos = file.readlines()
-            file.close()
+            with open("to_dos.txt", "r") as file:
+                to_dos = file.readlines()
             #new_to_do = [item.strip('\n') for item in to_dos] Previous code to remove the gap between items
             for index, x in enumerate(to_dos):
                 x = x.strip('\n')
                 print(f"{index + 1}. {x.capitalize()}")
         case "edit":
+            # We open the to do list .txt to store the values of the file on the variable to_dos
+            with open("to_dos.txt","r") as file:
+                to_dos = file.readlines()
+            # We print out the list so the user knows which to_do to edit
             for index, x in enumerate(to_dos):
+                x = x.strip('\n')
                 print(f"{index + 1}. {x.capitalize()}")
+            # Once the user knows the to_dos and their number, they can pick whichever the want to edit
             edit_to_do = int(input("Enter the number of the to do you wish to edit: "))
             edit_to_do = edit_to_do - 1
+            # After the player selects which one to edit, now the user can edit the to_do, asigning the value on new_to_do
             new_to_do = input("Enter the new to do: ")
-            to_dos[edit_to_do] = new_to_do
-            Amount_of_to_dos = 0
+            # And assigning the value to de selected to_do with edit_to_do and changing the value with the variable new_to_do
+            to_dos[edit_to_do] = new_to_do + '\n'
+            # We open the file to assign the new values to the .txt list
+            with open("to_dos.txt", "w") as file:
+                file.writelines(to_dos)
+            # We print out the new list so that the user knows how it looks now.
             print("The new list now looks like this: ")
-            for x in to_dos:
-                Amount_of_to_dos = Amount_of_to_dos + 1
-                print(f"{Amount_of_to_dos}. {x.capitalize()}")
-        case "complete":
             for index, x in enumerate(to_dos):
+                x = x.strip('\n')
                 print(f"{index + 1}. {x.capitalize()}")
+        case "complete":
+            # We open the .txt to get access to the current data on it
+            with open("to_dos.txt","r") as file:
+                to_dos = file.readlines()
+            # We print out the list with the number so that the user knows which one to select
+            for index, x in enumerate(to_dos):
+                x = x.strip('\n')
+                print(f"{index + 1}. {x.capitalize()}")
+            # We ask the user which one the user wants to remove
             item_to_remove = int(input("Enter the number of the task you completed: "))
+            # We remove it from the array
             to_dos.pop(item_to_remove - 1)
+            # We write the .txt file again with the removed item
+            with open("to_dos.txt", "w") as file:
+                file.writelines(to_dos)
+            # We show the user how the list looks after the change.
             print("Great, well done, now the list looks like this: ")
             for index, x in enumerate(to_dos):
+                x = x.strip('\n')
                 print(f"{index + 1}. {x.capitalize()}")
         case "exit":
             break
