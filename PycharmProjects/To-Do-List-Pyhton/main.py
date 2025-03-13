02ba7336bd553c3ff8ce1ef58699dc832c05827e
ESuances <erick.s.suances.arc@gmail.com>
2025-03-12 21:14:03 -0600

    Day 13 - Default Arguments
    Got Bored, decided to rush Day 13 today since its been kinda not too much information lately
    
    And I was not wrong, we learned how to place default arguments to a function, oh and that I can add """ to make a multiline string and comment, but meh, I can look for that anyday.

--- PycharmProjects/To-Do-List-Pyhton/main.py
+++ PycharmProjects/To-Do-List-Pyhton/main.py
@@ -1,9 +1,9 @@
-def get_to_dos(filepath):
+def get_to_dos(filepath="to_dos.txt"): # This way, the function already has a default parameter
     with open(filepath, "r") as file_local:
         to_dos_local = file_local.readlines()
     return to_dos_local
 
-def write_to_dos(filepath, to_dos_local):
+def write_to_dos(to_dos_local, filepath="to_dos.txt"): # If there is an argument without a default parameter, to fix the error, just put it first.
     with open(filepath, "w") as file_local:
         file_local.writelines(to_dos_local)
 
@@ -14,23 +14,23 @@ def printOutTo_dos(to_dos_local):
 
 def add_to_dos(decision_local):
     to_do = decision_local[4:]
-    to_dos = get_to_dos("to_dos.txt")
+    to_dos = get_to_dos() # Since we have a default parameter, we don't need to add the argument if the default is correct.
     to_dos.append(to_do + '\n')
-    write_to_dos("to_dos.txt", to_dos)
+    write_to_dos(to_dos)
 
 def show_to_dos():
-    to_dos = get_to_dos("to_dos.txt")
+    to_dos = get_to_dos()
     printOutTo_dos(to_dos)
 
 def edit_to_dos():
     try:  # Error handling
-        to_dos = get_to_dos("to_dos.txt")
+        to_dos = get_to_dos()
         printOutTo_dos(to_dos)
         edit_to_do = int(input("Enter the number of the to do you wish to edit: "))
         edit_to_do = edit_to_do - 1
         new_to_do = decision[5:]
         to_dos[edit_to_do] = new_to_do + '\n'
-        write_to_dos("to_dos.txt", to_dos)
+        write_to_dos(to_dos)
         print("The new list now looks like this: ")
         printOutTo_dos(to_dos)
     except ValueError:  # Cuando el user ingresa un valor no aceptado, se muestra este error
@@ -42,7 +42,7 @@ def complete_to_dos():
         printOutTo_dos(to_dos)
         item_to_remove = int(input("Enter the number of the task you completed: "))
         to_dos.pop(item_to_remove - 1)
-        write_to_dos("to_dos.txt", to_dos)
+        write_to_dos(to_dos)
         print("Great, well done, now the list looks like this: ")
         printOutTo_dos(to_dos)
     # Error handling, making sure if the program gets into any error, we let the user know
