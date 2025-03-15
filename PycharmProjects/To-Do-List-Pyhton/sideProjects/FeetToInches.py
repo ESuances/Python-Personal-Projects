16adde8f377b19a38f9c496bf3b6ac9bb9dc63d3
ESuances <erick.s.suances.arc@gmail.com>
2025-03-15 10:03:16 -0600

    Day 16 - GUI Starter
    Again, we started with GUI, in this case these were the added exercises that we made, it is a file zipper and a converter from feet to inches as the program says.
    
    I just love it.

---
+++ PycharmProjects/To-Do-List-Pyhton/sideProjects/FeetToInches.py
@@ -0,0 +1,15 @@
+import FreeSimpleGUI as sg
+
+label1 = sg.Text("Enter feet: ")
+input1 = sg.Input()
+label2 = sg.Text("Enter inches: ")
+input2 = sg.Input()
+
+compress_button = sg.Button("Convert")
+
+window = sg.Window("File Compresser",
+                   layout=[[label1, input1],
+                           [label2, input2],
+                           [compress_button]])
+window.read()
+window.close()
