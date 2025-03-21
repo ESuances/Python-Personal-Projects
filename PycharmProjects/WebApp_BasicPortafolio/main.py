c3d618400d0e723f6633f535a1f157d7abfea51b
ESuances <erick.s.suances.arc@gmail.com>
2025-03-20 18:25:19 -0600

    Day 20 - First Website - Portafolio Website

--- PycharmProjects/WebApp_BasicPortafolio/main.py
+++ PycharmProjects/WebApp_BasicPortafolio/main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="centered")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/MyCurrentProfile.png")
+    st.image("images/MyCurrentProfile.png", width=1500)
 
 with col2:
     st.title("Welcome to my first portafolio")
@@ -13,3 +13,5 @@ with col2:
     game develpoment and small React Native applications, but I also have experience in big and scalable applications Using PHP Laravel as backend with MySQL
     and React as the Front End."""
     st.info(content)
+
+st.text("Below you can find some of the apps I have built in Python. Feel free to contact me!")
