93a55503b34825d0d09e8c1afd1fffa00aeafd20
ESuances <erick.s.suances.arc@gmail.com>
2025-03-20 18:10:33 -0600

    DAY 20 - Starting Portafolio

---
+++ PycharmProjects/WebApp_BasicPortafolio/main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(layout="centered")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/MyCurrentProfile.png")
+
+with col2:
+    st.title("Welcome to my first portafolio")
+    content = """Hi, I'm Erick Suances, a Mechatronics/Software engineer that loves to code, I have multiple projects going on, mainly I do
+    game develpoment and small React Native applications, but I also have experience in big and scalable applications Using PHP Laravel as backend with MySQL
+    and React as the Front End."""
+    st.info(content)
