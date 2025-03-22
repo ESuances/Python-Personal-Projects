a7a587e9ef4c5793665b5a48f4c145805bf42aa7
ESuances <erick.s.suances.arc@gmail.com>
2025-03-22 14:08:06 -0600

    Update Contact_Me.py

--- PycharmProjects/WebApp_BasicPortafolio/pages/Contact_Me.py
+++ PycharmProjects/WebApp_BasicPortafolio/pages/Contact_Me.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.header("Contact Me")
+st.write("If you're interested in my work, please, send me a message, I'll gladly response as soon as possible.")
+
+with st.form(key='ContactForm'):
+    user_email = st.text_input("Your Email address")
+    user_message = st.text_area("Your message")
+    button = st.form_submit_button("Send")
+    #if button:
+
