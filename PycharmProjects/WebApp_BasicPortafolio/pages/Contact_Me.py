c5191880a21e2fe48dc23bf0b85ec6fb4b43c8d2
ESuances <erick.s.suances.arc@gmail.com>
2025-03-22 15:04:52 -0600

    Day 23 - Sent Emails from site
    Aprendi a ahora si poder mandar emails desde la pagina, aunque veo que si tiene varios requisitos que desafortunadamente obstaculizarian que haga la applicacion para el trabajo.
    
    Tendre que encontrar otra forma o investigar con ellos mismos que puerto, host y demas settings se necesitarian para algo asi.

--- PycharmProjects/WebApp_BasicPortafolio/pages/Contact_Me.py
+++ PycharmProjects/WebApp_BasicPortafolio/pages/Contact_Me.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from send_email import sendEmail
 
 st.header("Contact Me")
 st.write("If you're interested in my work, please, send me a message, I'll gladly response as soon as possible.")
@@ -6,6 +7,13 @@ st.write("If you're interested in my work, please, send me a message, I'll gladl
 with st.form(key='ContactForm'):
     user_email = st.text_input("Your Email address")
     user_message = st.text_area("Your message")
-    button = st.form_submit_button("Send")
-    #if button:
+    message = f"""\
+Subject: New Email from {user_email}
 
+Email received from {user_email}
+{user_message}
+    """
+    button = st.form_submit_button("Send")
+    if button:
+        sendEmail(message)
+        st.info("Your Email was sent successfully, I'll respond as soon as posible, Thanks!")
