d8f86bbbae42233a91f93380ba67c74010237cdb
ESuances <erick.s.suances.arc@gmail.com>
2025-03-23 12:31:39 -0600

    Day 24 - PDF creation and manipulation
    Creation of PDF files and manipulation of such files, awesome things, just not as flashy as I would like, but I'm getting good at algorithms myself, not the leetcode algos, but it is still something, I love it

--- PycharmProjects/Pdf-Template/main.py
+++ PycharmProjects/Pdf-Template/main.py
@@ -2,16 +2,31 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
     pdf.add_page()
     pdf.set_font(family="Times", style="B",size=24)
-    pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10,21,200,21)
+    for x in range(0, 270, 10):
+        pdf.line(10, x + 21, 200, x + 21)
+
+    # Set footer
+    pdf.ln(260)
+    pdf.set_font(family="Times", style="B", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1)
+
     for x in range(row["Pages"] - 1):
         pdf.add_page()
+        # Set footer
+        for i in range(0, 270, 10):
+            pdf.line(10, i + 21, 200, i + 21)
+        pdf.ln(270)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1)
 
 pdf.output("output.pdf")
