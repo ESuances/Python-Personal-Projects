02199bb4973534c69a7e22987a59219ce3bce89c
ESuances <erick.s.suances.arc@gmail.com>
2025-03-25 10:16:45 -0600

    Day 25 - Excel to PDF
    Obtained information from a PDF file name and generated PDF with the filename provided.

--- PycharmProjects/PDF_InvoiceGenerator/main.py
+++ PycharmProjects/PDF_InvoiceGenerator/main.py
@@ -2,9 +2,16 @@
 # To make a program, you have to think about the input data and the output
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 filepaths = glob.glob("Invoices/*xlsx") # The * means getting all the type of files in this case .xlsx
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1") # This is in particular from pandas to read Excel files.
-    print(df)
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    filename = Path(filepath).stem[:5] # We get the name of the file by using this library.
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{filename}") # Title of the PDF
+    pdf.output(f"GeneratedPDFs/{filename}.pdf") # Name of the PDF and where it would be outputed.
