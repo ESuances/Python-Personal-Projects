45aadd0ebacc578bb5b9300f6c10eb36326842cc
ESuances <erick.s.suances.arc@gmail.com>
2025-03-29 12:46:25 -0600

    Day 29 - Web API with Flask
    Just created an API for user's to get temperature with parameters, not finished but wanted to post progress.

--- PycharmProjects/BushCraftAPI/main.py
+++ PycharmProjects/BushCraftAPI/main.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
+import pandas as pd
 
-app = Flask('Website')
+app = Flask(__name__) # This takes the literal name of the file without .py
 
-@app.route('/home')
+@app.route('/') #This will be the starting page.
 def home():
-    return render_template('tutorial.html')
+    return render_template('home.html')
 
-@app.route('/about/')
-def about():
-    return render_template('about.html')
+@app.route("/api/v1/<station>/<date>")
+def api(station, date):
+    # df = pd.read_csv
+    temperature = 33
+    return {"station": station,
+            "date": date,
+            "temperature": temperature,}
 
-app.run(debug=True)
+if __name__ == "__main__": # This makes it so that only when we run this file, this code is actually executed, meaning creating the site on Flask
+    app.run(debug=True)
