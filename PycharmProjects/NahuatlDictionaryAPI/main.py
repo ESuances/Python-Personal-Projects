83fff06c994038e1a6912a2b83e472b45da94983
ESuances <erick.s.suances.arc@gmail.com>
2025-03-30 17:17:03 -0600

    Day 30

--- PycharmProjects/NahuatlDictionaryAPI/main.py
+++ PycharmProjects/NahuatlDictionaryAPI/main.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
+
+df = pd.read_csv('dictionary.csv') # No es un diccionario Nahuatl, despues lo tendre que cambiar
+
+# Sorry, la neta no me sentia tan motivado o mas bien tenia la mente en otro lado
+# La verdad no me motiva mucho esto de aprender a hacer APIs, pense que seria mucho mejor, pero es puro dato
+# Supongo que pense que era mas divertido manipular datos, pero hasta el momento, bastante meh
 
 @app.route('/')
 def home():
@@ -8,7 +15,7 @@ def home():
 
 @app.route("/api/v1/<palabra>")
 def api(palabra):
-    definition = palabra.upper()
+    definition = df.loc[df["palabra"] == palabra]['definition'].squeeze()
     result_dict = {"palabra": palabra, 'definition': definition}
     return result_dict
 
